72ca105206034b5a6e4c9ce14191a2c97f28c212
alu0100693737 <alu0100693737@ull.edu.es>
2016-02-29 19:55:28 +0100

    Problemas con JavaScript

--- temperature.js
+++ temperature.js
@@ -2,10 +2,10 @@
 
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
-exports.Expresion = function Expresion(valor, medida) {
-  this.valor = valor;
-  this.tipo = medida;
-  console.log(valor + " " + medida);
+exports.Expresion = function Expresion(num, tipo) {
+  this.num = num;
+  this.tipo = tipo;
+  console.log(num + " " + tipo);
   this.mensaje = function mensaje() {
     return "Recogido expresion correcta";
   }
@@ -13,14 +13,27 @@ exports.Expresion = function Expresion(valor, medida) {
 
 Converter.prototype = new Expresion();
 
-function Converter(valor, medida) {
-  this.valor = valor;
+function Converter(num, tipo) {
+  this.num = num;
   this.tipo = tipo;
   var result;
-  this.calcular = function calcular
+  this.calcular = function calcular(num, tipo) {
+    if (this.tipo == 'c' || this.tipo == 'C') {
+      result = (num * 9/5)+32;
+      result = result.toFixed(1)+" Farenheit";
+    }
+    else {
+      result = (num - 32)*5/9;
+      result = result.toFixed(1)+" Celsius"
+    }
+    return result;
+  }
 }
+
+exports.Converter = Converter;
+
 exports.calculate = function calculate() {
-  var result;
+
   var temp = original.value;
   //expresion regular. Recordar ?: matches
   var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([cCfF])((e|el|els|elsi|elsiu|elsius)|(a|ar|are|aren|arenh|arenhe|arenhei|arenheit))?\s*$/i;
@@ -34,7 +47,9 @@ exports.calculate = function calculate() {
     var num = m[1];
     var type = m[2];
     num = parseFloat(num);
+
     if (type == 'c' || type == 'C') {
+      var resultado = new Converter(num, type)
       result = (num * 9/5)+32;
       result = result.toFixed(1)+" Farenheit";
     }
@@ -42,28 +57,18 @@ exports.calculate = function calculate() {
       result = (num - 32)*5/9;
       result = result.toFixed(1)+" Celsius"
     }
-    for (var i = 0; i < m.length; i++) {
-
-    }
 
     document.getElementById("converted").innerHTML= result;
     return true;
-
-    //alert(result);
   }
   else {
     document.getElementById("converted").innerHTML= "ERROR! Try something like '-4.2C' instead";
-    return console.error("No encontrado matches");
+//var expresion = new Expresion(num, tipo);
+//deberia document.getElementById("converted").innerHTML = expresion.mostrar();
+    return console.log("No encontrado matches");
     //converted.innerHTML = "ERROR! Try something like '-8.2e-3 C' instead";
   }
 }
-
-
-export.calculate2 = function calculate2(prueba){
-    return console.error("missing target");
-}
-
-
 
 
 })(this);//clausura
