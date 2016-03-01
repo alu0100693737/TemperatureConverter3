7253fb522b410f0aee370b10a20986a1ea701e01
alu0100693737 <alu0100693737@ull.edu.es>
2016-03-01 12:20:16 +0100

    Agregados tests

--- tests/tests.js
+++ tests/tests.js
@@ -11,17 +11,17 @@ suite('temperature', function() {
     test('32F = 0C', function() {
         original.value = "32F";
         calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        assert.deepEqual(converted.innerHTML, "0.00 Celsius and 273.15 Kelvin");
     });
     test('12F = -11.1C', function() {
         original.value = "12F";
         calculate();
-        assert.deepEqual(converted.innerHTML, "-11.1 Celsius");
+        assert.deepEqual(converted.innerHTML, "-11.11 Celsius and 262.04 Kelvin");
     });
     test('6.1C = 43F', function() {
         original.value = "6.1C";
         calculate();
-        assert.deepEqual(converted.innerHTML, "43.0 Farenheit");
+        assert.deepEqual(converted.innerHTML, "42.98 Farenheit and 279.25 Kelvin");
     });
     test('3te = Numero introducido no valido', function() {
         original.value = "3te";
@@ -32,7 +32,7 @@ suite('temperature', function() {
     test('45C = 113.0 Farenheit', function() {
         original.value = "45C";
         calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        assert.deepEqual(converted.innerHTML, "113.00 Farenheit and 318.15 Kelvin");
     });
     test('5X = error', function() {
         original.value = "5X";
